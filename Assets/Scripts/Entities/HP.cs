f33a2969f54a4146cf4f08d53dfe10c8a41d6306
Luciano Donati <me@lucianodonati.com>
2020-04-29 23:59:26 -0400

    Added HP component that implements IDamageable

---
+++ Assets/Scripts/Entities/HP.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnityEngine;
+
+namespace Entities
+{
+    public class HP : IDamageable
+    {
+        private float currentHP;
+        private readonly float maxHP;
+
+        public bool IsAlive { get; private set; }
+
+        public float CurrentHP
+        {
+            get => currentHP;
+            set
+            {
+                currentHP = value;
+                if (currentHP <= 0)
+                {
+                    currentHP = 0;
+                    IsAlive = false;
+                }
+            }
+        }
+
+        public HP(float maxHP = 100)
+        {
+            if (maxHP < 0)
+                throw new Exception("Invalid parameters for creating an Entity.");
+
+            currentHP = this.maxHP = maxHP;
+
+            IsAlive = currentHP > 0;
+        }
+
+        public HP(float maxHP, float startingHP)
+        {
+            if (maxHP < startingHP)
+                throw new AttemptToSetMaxHPLowerThanHealthException();
+
+            this.maxHP = maxHP;
+            CurrentHP = startingHP;
+
+            IsAlive = currentHP > 0;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            if (!IsAlive)
+                throw new AttemptToInteractWhenDeadException();
+            CurrentHP -= damage;
+        }
+
+        public void TakeHealing(float healAmount)
+        {
+            if (!IsAlive)
+                throw new AttemptToInteractWhenDeadException();
+
+            CurrentHP = Mathf.Min(CurrentHP + healAmount, maxHP);
+        }
+
+        /// <summary>
+        /// Revives the Entity at <see cref="healthPercentage"/> % of its original maxHP (see <see cref="Entity(float)"/>).
+        /// </summary>
+        /// <param name="healthPercentage">% from 0f to 1f of maxHP to revive target Entity.</param>
+        public void Revive(float healthPercentage)
+        {
+            IsAlive = true;
+            CurrentHP = maxHP * healthPercentage;
+        }
+
+        public class AttemptToSetMaxHPLowerThanHealthException : Exception
+        {
+        }
+
+        public class AttemptToInteractWhenDeadException : Exception
+        {
+        }
+    }
+}
