ce46d08cbf9a032308fedc48bb85e82765be9a9a
Luciano Donati <me@lucianodonati.com>
2020-04-29 23:56:13 -0400

    Added IDamageable interface.

---
+++ Assets/Scripts/Entities/IDamageable.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities
+{
+    public interface IDamageable
+    {
+        bool IsAlive { get; }
+        float CurrentHP { get; set; }
+        void TakeDamage(float damage);
+        void TakeHealing(float healAmount);
+        void Revive(float healthPercentage);
+    }
+}
