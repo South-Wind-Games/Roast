653eabc9555ec33f67e8b16c3256f152e04ad7d8
Arzenus <nicolastarulli@hotmail.com>
2020-05-06 17:51:44 -0300

    Add InputManager and RoastController

---
+++ Assets/Scripts/Roasts/Input/InputManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Roasts.Input
+{
+    enum InputMode
+    {
+        Default,
+        Warlocks,
+        Joystick
+    };
+    
+    public class InputManager : MonoBehaviour
+    {
+        private InputMode currentInputMode = InputMode.Default;
+        
+        private Vector2 m_MoVe;
+        
+        
+
+        public void OnMove (InputAction.CallbackContext context)
+        {
+            m_MoVe = context.ReadValue<Vector2>();
+        }
+
+        public void OnRightClicked(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnRocketFire(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnSelfBomb(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnSkill1(InputAction.CallbackContext context)
+        {
+            
+        }
+        
+        public void OnSkill2(InputAction.CallbackContext context)
+        {
+            
+        }
+        
+        public void OnSkill3(InputAction.CallbackContext context)
+        {
+            
+        }
+        
+        public void OnSkill4(InputAction.CallbackContext context)
+        {
+            
+        }
+        
+        public void OnSkill5(InputAction.CallbackContext context)
+        {
+            
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Roasts/Input/RoastController.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Roasts.Input
+{
+
+    public class RoastController : MonoBehaviour
+    {
+        private float moveSpeed;
+        private float moveRotation;
+
+        private Vector2 m_MoveTowards;
+        private Vector2 m_MoveToPosition;
+        private Vector2 m_LookAt;
+
+        public void MoveTowards(Vector2 context)
+        {
+            m_MoveTowards = context;
+        }
+
+        public void MoveToPosition()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            moveSpeed = 0;
+        }
+
+        public void StopAndLookAt()
+        {
+            Stop();
+
+            
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }    
+}
+
