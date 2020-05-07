8ae62bccc26ba5ccfe7ea06da12c6322a8e6eeba
Luciano Donati <me@lucianodonati.com>
2020-05-06 20:30:09 -0400

    Made InputManager require a RoastController. Called RoastController's move when move input detected.

--- Assets/Scripts/Roasts/Input/InputManager.cs
+++ Assets/Scripts/Roasts/Input/InputManager.cs
@@ -1,68 +1,72 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Roasts.Input
 {
-    enum InputMode
-    {
-        Default,
-        Warlocks,
-        Joystick
-    };
-    
+	// TODO: It needs to require SkillsController
+    [RequireComponent(typeof(RoastController))]
     public class InputManager : MonoBehaviour
     {
-        private InputMode currentInputMode = InputMode.Default;
-        
-        private Vector2 m_MoVe;
-        
-        
-
-        public void OnMove (InputAction.CallbackContext context)
+        enum InputMode
         {
-            m_MoVe = context.ReadValue<Vector2>();
+            Default,
+            Warlocks,
+            Joystick
+        };
+
+        private InputMode currentInputMode = InputMode.Default;
+
+        #region Auto-Reference
+
+        [SerializeField, HideInInspector] private RoastController roastController = null;
+
+        private void OnValidate()
+        {
+            if (null == roastController)
+            {
+                roastController = GetComponent<RoastController>();
+            }
+        }
+
+        #endregion
+
+        public void OnMoveDirection(InputAction.CallbackContext context)
+        {
+            var rawInput = context.ReadValue<Vector2>();
+            roastController.MoveInDirection(new Vector3(rawInput.x, 0, rawInput.y));
         }
 
         public void OnRightClicked(InputAction.CallbackContext context)
         {
-            
         }
 
         public void OnRocketFire(InputAction.CallbackContext context)
         {
-            
         }
 
         public void OnSelfBomb(InputAction.CallbackContext context)
         {
-            
         }
 
         public void OnSkill1(InputAction.CallbackContext context)
         {
-            
         }
-        
+
         public void OnSkill2(InputAction.CallbackContext context)
         {
-            
         }
-        
+
         public void OnSkill3(InputAction.CallbackContext context)
         {
-            
         }
-        
+
         public void OnSkill4(InputAction.CallbackContext context)
         {
-            
         }
-        
+
         public void OnSkill5(InputAction.CallbackContext context)
         {
-            
         }
     }
 }
-
-
