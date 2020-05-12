42e3fcd6e2ff0612bf4e796956ae862caee00cf6
Arzenus <nicolastarulli@hotmail.com>
2020-05-12 17:16:50 -0300

    removed character controller

--- Assets/Scripts/Roasts/RoastController.cs
+++ Assets/Scripts/Roasts/RoastController.cs
@@ -5,32 +5,22 @@ using UnityEngine;
 namespace Roasts
 {
     [Serializable]
-    [RequireComponent(typeof(CharacterController))]
     [RequireComponent(typeof(Mirror.NetworkTransform))]
     public class RoastController : Mirror.NetworkBehaviour
     {
-        // TODO: Convert to auto-reference.
-        public CharacterController characterController;
-
+       
         [TabGroup("Movement Settings"), SerializeField]
         private float moveSpeed = 10, rotationSpeed = 1;
 
         [TabGroup("Diagnostics"), ShowInInspector, ReadOnly]
         private Vector3 direction;
 
-        #region Auto-References
-
-        void OnValidate()
-        {
-            if (characterController == null)
-                characterController = GetComponent<CharacterController>();
-        }
-
-        #endregion
-
+     
         public void MoveInDirection(Vector3 inputDirection)
         {
             direction = inputDirection;
+
+
         }
 
         public void MoveToPosition(Vector2 mousePosition)
@@ -46,12 +36,20 @@ namespace Roasts
             Stop();
         }
 
+        /*private void Update()
+        {
+            
+        }*/
+
         private void FixedUpdate()
         {
             if (!isLocalPlayer)
                 return;
+            
 
-            transform.Translate(direction * (moveSpeed * Time.fixedDeltaTime));
+
+            // transform.Translate(direction * (moveSpeed * Time.fixedDeltaTime), 
+                // Space.World);
             // TODO: Player needs to rotate to face the direction he's moving.
         }
     }
