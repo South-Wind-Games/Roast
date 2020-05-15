f9600fc0238d4e01e640492a77c761b0bb7ea6ec
Luciano Donati <me@lucianodonati.com>
2020-05-15 12:24:06 -0300

    Base server running

---
+++ Server/GameServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Roast Game Server";
+
+            Server.Start(6, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
