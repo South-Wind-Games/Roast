270732ad478f4e05c9c8f30ebd9706a4960d6cda
Luciano Donati <me@lucianodonati.com>
2020-05-15 12:23:50 -0300

    Added Client class

---
+++ Server/GameServer/Client.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+
+        public int id;
+        public TCP tcp;
+
+        public Client(int _id)
+        {
+            id = _id;
+            tcp = new TCP(id);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+            private readonly int id;
+
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+
+                receiveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+                // TODO: WelcomePacket?
+            }
+
+            private void ReceiveCallback(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = stream.EndRead(_result);
+                    if (_byteLength <= 0)
+                        // TODO: Disconnect
+                        return;
+                    else
+                    {
+                        byte[] _data = new byte[_byteLength];
+                        Array.Copy(receiveBuffer, _data, _byteLength);
+
+                        // TODO: Handle data
+                        stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error receiving TCP data: {e}");
+                }
+            }
+        }
+    }
+}
