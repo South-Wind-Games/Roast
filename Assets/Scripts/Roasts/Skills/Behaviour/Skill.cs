a9eb1f47a5122250c098db1c7d33daf1bc7f22d1
EnzoR98 <enzo.rolon@outlook.com>
2020-05-16 19:07:42 -0300

    Added abstract Skill class

---
+++ Assets/Scripts/Roasts/Skills/Behaviour/Skill.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Roasts;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public abstract class Skill : MonoBehaviour
+{
+    protected RoastPlayer owner;
+    //protected T data;
+    protected int currentLevel;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public abstract void Use();
+}
