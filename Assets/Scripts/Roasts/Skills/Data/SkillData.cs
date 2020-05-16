62f17f8ffa01029f39b2cab1e03a3db77ebf69f7
EnzoR98 <enzo.rolon@outlook.com>
2020-05-16 19:58:28 -0300

    Added ScriptableObject SkillData class

---
+++ Assets/Scripts/Roasts/Skills/Data/SkillData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillData : ScriptableObject
+{
+    public int goldCost;
+    public float coolDownTime;
+    public float significantAmount;
+    public AnimationCurve[] levelIncreaseCurves;
+}
