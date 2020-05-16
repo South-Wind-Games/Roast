c2d3969b2a97f0768e8ed8d5e420989b72faf4ce
EnzoR98 <enzo.rolon@outlook.com>
2020-05-16 19:41:59 -0300

    Added abstract Others class which inherit from Skill

---
+++ Assets/Scripts/Roasts/Skills/Behaviour/Others.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Roasts;
+using UnityEngine;
+
+public abstract class Others : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected void OnTriggerEnter(Collider other) {}
+    protected void OnTriggerStay(Collider other) {}
+    protected void OnTriggerExit(Collider other) {}
+    protected void OnPlayerEnter(RoastPlayer other) {}
+    protected void OnPlayerStay(RoastPlayer other) {}
+    protected void OnPlayerExit(RoastPlayer other) {}
+    protected void OnSkillEnter(Skill other) {}
+    protected void OnSkillStay(Skill other) {}
+    protected void OnSkillExit(Skill other) {}
+}
