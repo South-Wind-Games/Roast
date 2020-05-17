a0fe35535cf8fc32a9d3b198e7e2357d9834fab7
Luciano Donati <me@lucianodonati.com>
2020-05-16 22:59:12 -0300

    Created actual skills scripts

---
+++ Assets/Scripts/Roasts/Skills/Actual/Rocket.cs
@@ -0,0 +1,11 @@
+﻿using Roasts.Skills.Data;
+
+namespace Roasts.Skills.Actual
+{
+    public class Rocket : Skill<ProjectileData>
+    {
+        public override void Use()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Roasts/Skills/Actual/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Roasts.Skills.Actual
+{
+    [Serializable]
+    public class Teleport : Skill<SkillData>
+    {
+        [Button]
+        public override void Use()
+        {
+            Debug.Log($"Me movi {data.significantAmount * data.levelIncreaseCurves[0].Evaluate(currentLevel * .1f)}");
+        }
+    }
+}
