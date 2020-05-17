5f9b0c8a387cd4940fb27a44999bbdcf15bc0976
Luciano Donati <me@lucianodonati.com>
2020-05-16 22:58:26 -0300

    Created scriptable objects assets

---
+++ Assets/Scripts/Roasts/Skills/Data/ProjectileData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Roasts.Skills.Data
+{
+    [CreateAssetMenu(fileName = "NewProjectile", menuName = "GameSettings/SkillData/Create new ProjectileData", order = 1)]
+    public class ProjectileData : SkillData
+    {
+        public float speed = 1;
+    }
+}
