d732ce18985b723a07fbb7bb1d5843a57bbd7a5f
Luciano Donati <me@lucianodonati.com>
2020-05-16 22:57:23 -0300

    Added create asset code. Added odin attributes as examples.

--- Assets/Scripts/Roasts/Skills/Data/SkillData.cs
+++ Assets/Scripts/Roasts/Skills/Data/SkillData.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class SkillData : ScriptableObject
+// TODO: Add SkillData to the correct namespace.
+[CreateAssetMenu(fileName = "NewSkillData", menuName = "GameSettings/SkillData/Create new SkillData", order = 1)]
+public class SkillData : SerializedScriptableObject
 {
-    public int goldCost;
+    [PropertyRange(1, 100), InfoBox("This is the cost in gold coins for this skill at this level.")]
+    public int goldCost = 1;
     public float coolDownTime;
     public float significantAmount;
     public AnimationCurve[] levelIncreaseCurves;
