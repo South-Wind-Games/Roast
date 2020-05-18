f099c8aae555e2c6657fa5ec263ad9325305fb1e
Santiago Aguirre <santi.aguirre10@gmail.com>
2020-05-18 16:55:23 -0300

    Added the script Merchant

---
+++ Assets/Scripts/Roasts/Item/Merchant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchant : MonoBehaviour
+{
+    protected ItemManager itemManager;
+
+    public Item Buy(int a)
+    {
+        return null;
+    } 
+    
+}
