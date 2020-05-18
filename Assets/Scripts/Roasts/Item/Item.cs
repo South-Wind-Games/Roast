621207d1166f28bdfae8f8c80bee6ddba8717d0e
Santiago Aguirre <santi.aguirre10@gmail.com>
2020-05-18 16:56:09 -0300

    Added the script Item

---
+++ Assets/Scripts/Roasts/Item/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Roasts;
+using UnityEngine;
+
+public class Item
+{
+    private Effect[] effects;
+
+    public void ApplyEffects(RoastPlayer roastPlayer)
+    {
+        
+    }
+    public void DisableEffects(RoastPlayer roastPlayer)
+    {
+        
+    }
+}
