466f7dbd55f6dd7f9f2d44911bbbf2bc8f4848b5
Santiago Aguirre <santi.aguirre10@gmail.com>
2020-05-18 16:55:37 -0300

    Added the script ItemManager

---
+++ Assets/Scripts/Roasts/Item/ItemManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ItemManager
+{
+    public Dictionary<ItemData, Item> items;
+
+    public ItemData[] GetAllItemsData()
+    {
+        return null;
+    }
+
+    public Item GetItemFromData(ItemData itemData)
+    {
+        return null;
+    }
+}
