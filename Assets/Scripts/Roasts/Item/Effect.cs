3f410f8295b114728440e970b1aa374eea35d261
Santiago Aguirre <santi.aguirre10@gmail.com>
2020-05-18 16:57:12 -0300

    Added the script Effect

---
+++ Assets/Scripts/Roasts/Item/Effect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Roasts;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+public abstract class Effect
+{
+    private float amount;
+    
+    public abstract void ApplyEffect(RoastPlayer roastPlayer);
+    
+    public abstract void DisableEffect(RoastPlayer roastPlayer);
+}
