7b52247a596318dcf3dcfd73e6b9acca2c880159
Santiago Aguirre <santi.aguirre10@gmail.com>
2020-05-18 16:55:59 -0300

    Added the script ItemData

---
+++ Assets/Scripts/Roasts/Item/ItemData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : ScriptableObject
+{
+    public enum SlotType
+    {
+        Boots
+    }
+    public string name;
+    public SlotType slot;
+    public string description;
+    public Item[] requeriments;
+    public int price;
+}
