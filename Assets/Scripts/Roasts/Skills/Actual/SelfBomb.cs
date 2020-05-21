ef5ee1e4c330d8a4a3ddb31f63a7bbd13ec51225
EnzoR98 <enzo.rolon@outlook.com>
2020-05-21 18:26:23 -0300

    Added SelfBomb behaviour class

---
+++ Assets/Scripts/Roasts/Skills/Actual/SelfBomb.cs
@@ -0,0 +1,16 @@
+﻿using Roasts.Skills.Behaviour;
+using Roasts.Skills.Data;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Roasts.Skills.Actual
+{
+    public class SelfBomb : Skill<OthersData>
+    {
+        [Button]
+        public override void Use()
+        {
+            
+        }
+    }
+}
