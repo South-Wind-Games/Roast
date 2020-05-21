ea3cc166aeb0604b4864380e9cf7624ad24f133c
Arzenus <nicolastarulli@hotmail.com>
2020-05-20 21:01:11 -0300

    Cleaned out using statements

--- Assets/Scripts/Roasts/RoastController.cs
+++ Assets/Scripts/Roasts/RoastController.cs
@@ -2,9 +2,6 @@ using System;
 using System.Collections;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
-using Button = UnityEngine.UI.Button;
 
 namespace Roasts
 {
