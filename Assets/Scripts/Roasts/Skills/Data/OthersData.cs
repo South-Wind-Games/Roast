7f7767a0d3b57025be6ec0220d77e03bdb7a864a
EnzoR98 <enzo.rolon@outlook.com>
2020-05-21 18:15:37 -0300

    Added OthersData class

---
+++ Assets/Scripts/Roasts/Skills/Data/OthersData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Roasts.Skills.Data
+{
+    [CreateAssetMenu(fileName = "OthersData", menuName = "GameSettings/SkillData/Create new OthersData", order = 2)]
+    public class OthersData : SkillData
+    {
+        public float radius;
+        public float pushingDistance;
+    }
+}
