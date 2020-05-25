a8b11ca8e0e46478c1325ab7fed21c9046a72a19
EnzoR98 <enzo.rolon@outlook.com>
2020-05-25 20:32:22 -0300

    Added Merchant.cs and created OnPurchase method to assign a skill to a player

---
+++ Assets/Scripts/Roasts/Skills/Merchant.cs
@@ -0,0 +1,23 @@
+﻿using Roasts.Skills.Data;
+using UnityEngine;
+
+namespace Roasts.Skills
+{
+    public class Merchant : MonoBehaviour
+    {
+        [SerializeField] 
+        private ScriptableObject skillList;
+        
+        public void OnPurchase(SkillData skill, RoastPlayer buyer)
+        {
+            if (buyer.Gold >= skill.goldCost)
+            {
+                buyer.GiveSkill(skill);
+            }
+            else
+            {
+                print("Not enough gold");
+            }
+        }
+    }
+}
