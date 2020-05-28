10050428fa4408098c53a97666f45610477725ed
Luciano Donati <me@lucianodonati.com>
2020-05-28 20:10:00 -0300

    Added basic skill functionality to SKillBase. Skill only adds the generic T variable.

--- Assets/Scripts/Roasts/Skills/Behaviour/Skill.cs
+++ Assets/Scripts/Roasts/Skills/Behaviour/Skill.cs
@@ -8,25 +8,5 @@ namespace Roasts.Skills.Behaviour
     {
         [SerializeField, InlineEditor]
         protected T data;
-
-        [ShowInInspector, ReadOnly, ShowIf(nameof(IsApplicationRunning))]
-        protected RoastPlayer Owner;
-
-        protected int CurrentLevel { get; private set; } = 1;
-
-        [Button, ShowIf(nameof(IsApplicationRunning))]
-        public void Use(RoastPlayer owner, int level)
-        {
-            Owner = owner;
-            CurrentLevel = level;
-            OnSkillUse();
-        }
-
-        private bool IsApplicationRunning()
-        {
-            return Application.isPlaying;
-        }
-
-        protected abstract void OnSkillUse();
     }
 }
