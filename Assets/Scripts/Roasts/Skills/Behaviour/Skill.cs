e8dd1523cb1340f9905d18474414134f98358ebf
Engranpa <l.p.2.c_s@hotmail.com>
2020-05-28 19:55:23 -0300

    hardcode of the skill´s owner to test TP

--- Assets/Scripts/Roasts/Skills/Behaviour/Skill.cs
+++ Assets/Scripts/Roasts/Skills/Behaviour/Skill.cs
@@ -16,8 +16,8 @@ namespace Roasts.Skills.Behaviour
 
         [Button, ShowIf(nameof(IsApplicationRunning))]
         public void Use(RoastPlayer owner, int level)
-        {
-            Owner = owner;
+        {  Owner = GetComponentInParent<RoastPlayer>(); //Esto es Para probar los skills. Hay que codear que siempre que se instacie un skill sea hijo del player. PD puto el que lee!
+           // Owner = owner;
             CurrentLevel = level;
             OnSkillUse();
         }
