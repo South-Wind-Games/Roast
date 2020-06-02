92b277be9b4cd20289fc010e4ce1013c2a377ebd
Santiago Aguirre <santi.aguirre10@gmail.com>
2020-06-02 14:41:34 -0300

    Added the script Stats

---
+++ Assets/Scripts/Roasts/Item/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roasts.Input
+{
+    public class Stats : MonoBehaviour
+    {
+        public float coolDownPercentageReduction;
+        public float movementSpeedModification;
+        public float hpFlatModification;
+        public float hpPercentageModifiaction;
+        public float rangeModifiers;
+        public float damageModifers;
+    }
+}
