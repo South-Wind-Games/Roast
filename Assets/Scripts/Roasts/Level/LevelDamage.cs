0566fa067553158d26254d7c6ea62386f3c3f056
FJFFH <59668794+FJFFH@users.noreply.github.com>
2020-06-26 16:59:49 -0300

    ShrinkingLevel (sin limpieza)
    listo para limpiar el cofigo

---
+++ Assets/Scripts/Roasts/Level/LevelDamage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Roasts.Level
+{
+    public class LevelDamage : Level
+    {
+        RoastPlayer player;
+
+        bool imHurting = false;
+
+        float DPS;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            imHurting = false;
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            player = collision.gameObject.GetComponent<RoastPlayer>();
+
+            imHurting = true;
+
+            player.TakeDamage(DPS);
+
+        }
+
+        IEnumerator AffectPlayerRoutine()
+        {
+            while (player.CurrentHP > 0 && imHurting == true)
+            {
+                player.TakeDamage(DPS);
+            }
+
+            yield return null;
+        }
+
+    }
+}
--- Assets/Scripts/Roasts/Level/ShrinkingLevel.cs
+++ Assets/Scripts/Roasts/Level/ShrinkingLevel.cs
@@ -8,44 +8,66 @@ namespace Roasts.Level
 
     public class ShrinkingLevel : Level
     {
+        [SerializeField]
+        private AnimationCurve shrinkingCurve;
 
-        AnimationCurve w;
+        [SerializeField]
+        private float totalShrinkTime;
 
-        public float shrinkingSpeed;
+        [SerializeField]
+        Vector2 scaleFromTo = new Vector2(1, .1f);
 
-        float speedDecreasePercentage;
+        [SerializeField]
+        Vector3 scaleAxis = new Vector3(1, 0, 1);
 
-        float DPS;
-
-        public Vector3 scaleIn = new Vector3(1, 0, 1);
-
-
-        public void Start()
+        void Start()
         {
-            StartCoroutine(LevelReduction());   
 
-        }
+            //curve = new AnimationCurve(new Keyframe(1, 0), new Keyframe(1, 5));
+            shrinkingCurve.preWrapMode = WrapMode.Default;
+            shrinkingCurve.postWrapMode = WrapMode.Default;
 
-        private void Update()
-        {
-            // w.Evaluate(dominio) = imagen
-
-       
+            StartCoroutine(LevelReduction());
         }
 
         IEnumerator LevelReduction()
-            {
+        {
+            // for (float shrinkingSpeed = 1f; transform.localScale.x >= PorcentajeFinal*0.01+0.001; shrinkingSpeed -= 0.001f)
+            /* for (float shrinkingSpeed = 1f; transform.localScale.x >= PorcentajeFinal * 0.01 + 0.001; shrinkingSpeed -= 0.001f)
+             {
 
-            for(int i = 0; transform.localScale.x >= 0.5; i++)
+                 scaleIn = transform.localScale;
+                 gameObject.transform.localScale = new Vector3(shrinkingCurve.Evaluate(shrinkingSpeed), 0, shrinkingCurve.Evaluate(shrinkingSpeed));
+                 //gameObject.transform.localScale = scaleIn * shrinkingSpeed.Evaluate(3f);
+
+                 yield return null;
+                 // yield return new WaitForSeconds(1);
+             }
+             */
+            float timeEllapsed = 0, ratio = 0 ;
+
+            gameObject.transform.localScale = scaleAxis * scaleFromTo.x;
+
+            do
             {
-                transform.localScale = scaleIn - new Vector3(shrinkingSpeed, 0, shrinkingSpeed) * Time.deltaTime;
+                
+                timeEllapsed += Time.deltaTime;
+
+                float curveEvaluate = shrinkingCurve.Evaluate(ratio = timeEllapsed / totalShrinkTime);
+
+                gameObject.transform.localScale = scaleAxis * (scaleFromTo.x + curveEvaluate * (scaleFromTo.y - scaleFromTo.x));
+
+                yield return null;
             }
+            while (ratio < 1);
 
-        
-            yield return null;
+            gameObject.transform.localScale = scaleAxis * scaleFromTo.y;
+
+
+
+
 
         }
-
 
     }
 }
