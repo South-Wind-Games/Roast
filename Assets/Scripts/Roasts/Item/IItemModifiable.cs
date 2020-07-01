b82b29ca1b6e04b82baf468329c50efd0e4ae6cf
Santiago Aguirre <Santiaguirre10@users.noreply.github.com>
2020-07-01 19:56:11 -0300

    Added the interface IItemModifiable

---
+++ Assets/Scripts/Roasts/Item/IItemModifiable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roasts.Item
+{
+    public interface IItemModifiable<T> where T:Effect
+    {
+        void ApplyItem(T effect);
+
+        void UnApplyItem(T effect);
+    }
+}
+
